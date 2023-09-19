d675edc5dcf449a9e039642faebf81d90c4db4cb
sumitkum1990 <sumitkum1990@gmail.com>
2023-09-20 01:34:26 +0530

    first commit

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
 from flask import Flask
-
+from flask import request
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h1>Hello, World!</h1>"
+    return "Hello, World!"
+
+
+@app.route("/hello1")
+def hello_world1():
+    return "Hello, World1!"
+
+@app.route("/hello2")
+def hello_world2():
+    return "Hello, World2!"
+
+@app.route('/test_fun')
+def test():
+    a = 5+6
+    return "this is my first function in flask {}".format(a)
+
+@app.route('/input_url')
+def request_input():
+    data = request.args.get('x')
+    return "this is my input from url {}".format(data)
+
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
